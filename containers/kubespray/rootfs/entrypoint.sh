6b495d5b78ce4bd5f5ed17a6649792686aac6e59
usrbinkat <usrbinkat@braincraft.io>
2021-10-16 18:40:22 -0700

    add node labels
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/bash -x
+
 declare -a IPS="${HOSTS}"
 /root/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
+
 ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml
 ansible-playbook $@ --become ./cluster.yml 
+
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config 
 cat /etc/ansible/artifacts/admin.conf > /config 
+
 kubectl apply -f /root/patch/multus-daemonset.yml
+
+kubectl taint nodes --all --overwrite node-role.kubernetes.io/master-
+
+kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/master=''
+kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/control-plane=''
+
+kubectl label nodes --all --overwrite node-role.kubernetes.io/worker=''
+
+sleep 6
+
+kubectl get po -A
