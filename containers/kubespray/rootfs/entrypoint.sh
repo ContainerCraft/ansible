cbd071f4dab9d0432b4e1581c02610329dc1a697
usrbinkat <usrbinkat@braincraft.io>
2021-10-16 17:41:49 -0700

    test automation
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+declare -a IPS="${HOSTS}"
+/root/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
 ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml
 ansible-playbook $@ --become ./cluster.yml 
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config 
