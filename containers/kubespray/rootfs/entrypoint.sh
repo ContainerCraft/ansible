37b78b6c3d201fa61a72f07dad27b5c1444af3d9
usrbinkat <usrbinkat@braincraft.io>
2021-10-16 18:22:39 -0700

    add multus patch
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -5,3 +5,4 @@ ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml
 ansible-playbook $@ --become ./cluster.yml 
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config 
 cat /etc/ansible/artifacts/admin.conf > /config 
+kubectl apply -f /root/patch/multus-daemonset.yml
