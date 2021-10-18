8b4a199d857fbabee8dcd990b9f067b2f382fdbd
usrbinkat <usrbinkat@braincraft.io>
2021-10-17 21:50:29 -0700

    update entrypoint
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -5,6 +5,7 @@ declare -a IPS="${HOSTS}"
 
 ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml
 ansible-playbook $@ --become ./cluster.yml 
+ansible-playbook $@ --become ./cluster.yml 
 
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config 
 cat /etc/ansible/artifacts/admin.conf > /config 
@@ -21,6 +22,6 @@ kubectl label nodes --all --overwrite node-role.kubernetes.io/worker=''
 kubectl patch deployment -n kube-system coredns --patch='{"spec":{"template":{"spec":{"tolerations":[]}}}}'
 kubectl -n kube-system rollout restart deployment/coredns
 
-sleep 6
+sleep 7
 
 kubectl get po -A
