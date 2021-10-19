e76c6e7632f30d66e858434be1208f2f91b3b5db
usrbinkat <usrbinkat@braincraft.io>
2021-10-19 09:13:21 -0700

    fix missing zram package remove and fqdn var
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -7,35 +7,27 @@ cat <<EOF
 
 Deployment Config:
 
-  $KUBE_API_DNS=api.kargo.home.arpa
-  $VRRP_IP=192.168.1.60
-  $IPS
+  KUBE_API_FQDN = $KUBE_API_FQDN
+  VRRP_IP       = $VRRP_IP
+  IPS           = $IPS
 
 EOF
 sleep 1
 
 /root/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
 
-ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml || exit 1
+ansible-playbook $@ --extra-vars @/etc/ansible/vars.yml --become /root/.ansible/playbooks/kubespray-prep.yml || exit 1
 
-ansible-playbook $@ --become ./cluster.yml || ansible-playbook $@ --become ./cluster.yml
+ansible-playbook $@ --extra-vars @/etc/ansible/vars.yml --become ./cluster.yml || ansible-playbook $@ --extra-vars @/etc/ansible/vars.yml --become ./cluster.yml
 
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config
-
 kubectl apply -f /root/patch/multus-daemonset.yml
-
 kubectl taint nodes --all --overwrite node-role.kubernetes.io/master-
-
 kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/master=''
 kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/control-plane=''
-
 kubectl label nodes --all --overwrite node-role.kubernetes.io/worker=''
-
 kubectl patch deployment -n kube-system coredns --patch='{"spec":{"template":{"spec":{"tolerations":[]}}}}'
 kubectl -n kube-system rollout restart deployment/coredns
-
 sleep 7
-
 kubectl get po -A
-
-cat /etc/ansible/artifacts/admin.conf | sed "s/$VRRP_IP/$KUBE_API_DNS/g" | tee /config
+cat /etc/ansible/artifacts/admin.conf | sed "s/$VRRP_IP/$KUBE_API_FQDN/g" | tee /config
