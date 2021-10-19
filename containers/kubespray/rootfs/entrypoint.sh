5b7a209d3a4374fb0bff7f903f4f3b02570cdf8d
usrbinkat <usrbinkat@braincraft.io>
2021-10-19 08:14:44 -0700

    update entrypoint
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -1,14 +1,26 @@
 #!/bin/bash -x
+# ansible-playbook --user fedora -e ansible_ssh_pass=fedora -e ansible_sudo_pass=fedora --become ./cluster.yml
 
 declare -a IPS="${HOSTS}"
+
+cat <<EOF
+
+Deployment Config:
+
+  $KUBE_API_DNS=api.kargo.home.arpa
+  $VRRP_IP=192.168.1.60
+  $IPS
+
+EOF
+sleep 1
+
 /root/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
 
 ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml || exit 1
 
-ansible-playbook $@ --become ./cluster.yml || ansible-playbook $@ --become ./cluster.yml 
+ansible-playbook $@ --become ./cluster.yml || ansible-playbook $@ --become ./cluster.yml
 
-cat /etc/ansible/artifacts/admin.conf > /root/.kube/config 
-cat /etc/ansible/artifacts/admin.conf > /config 
+cat /etc/ansible/artifacts/admin.conf > /root/.kube/config
 
 kubectl apply -f /root/patch/multus-daemonset.yml
 
@@ -25,3 +37,5 @@ kubectl -n kube-system rollout restart deployment/coredns
 sleep 7
 
 kubectl get po -A
+
+cat /etc/ansible/artifacts/admin.conf | sed "s/$VRRP_IP/$KUBE_API_DNS/g" | tee /config
