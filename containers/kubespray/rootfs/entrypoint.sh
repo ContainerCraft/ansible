f5a969b7f2142a31f3b2a9e8d7a9c594d5184248
usrbinkat <usrbinkat@braincraft.io>
2021-10-18 17:01:17 -0700

    update cloud-init wait task
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -3,9 +3,9 @@
 declare -a IPS="${HOSTS}"
 /root/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
 
-ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml
-ansible-playbook $@ --become ./cluster.yml 
-ansible-playbook $@ --become ./cluster.yml 
+ansible-playbook $@ --become /root/.ansible/playbooks/kubespray-prep.yml || exit 1
+
+ansible-playbook $@ --become ./cluster.yml || ansible-playbook $@ --become ./cluster.yml 
 
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config 
 cat /etc/ansible/artifacts/admin.conf > /config 
