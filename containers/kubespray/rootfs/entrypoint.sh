0146ca1929925250c85b2d3ade9fda5b4680f99a
usrbinkat <usrbinkat@braincraft.io>
2021-10-20 10:23:39 -0700

    update entrypoint
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -18,17 +18,18 @@ sleep 1
 /root/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
 
 ansible-playbook $@ --extra-vars @/etc/ansible/vars.yml --become /root/.ansible/playbooks/kubespray-prep.yml || exit 1
-
 ansible-playbook $@ --extra-vars @/etc/ansible/vars.yml --become ./cluster.yml || ansible-playbook $@ --extra-vars @/etc/ansible/vars.yml --become ./cluster.yml
 
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config
+kubectl scale deployment --replicas=0 dns-autoscaler --namespace=kube-system
 kubectl apply -f /root/patch/multus-daemonset.yml
 kubectl taint nodes --all --overwrite node-role.kubernetes.io/master-
 kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/master=''
 kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/control-plane=''
 kubectl label nodes --all --overwrite node-role.kubernetes.io/worker=''
-kubectl patch deployment -n kube-system coredns --patch='{"spec":{"template":{"spec":{"tolerations":[]}}}}'
-kubectl -n kube-system rollout restart deployment/coredns
+#kubectl patch deployment -n kube-system coredns --patch='{"spec":{"template":{"spec":{"tolerations":[]}}}}'
+#kubectl -n kube-system rollout restart deployment/coredns
 sleep 7
+kubectl apply -f /root/patch/multus-daemonset.yml
 kubectl get po -A
 cat /etc/ansible/artifacts/admin.conf | sed "s/$VRRP_IP/$KUBE_API_FQDN/g" | tee /config
