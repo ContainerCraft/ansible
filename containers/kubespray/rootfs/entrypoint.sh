2d7a2563793ac1f8f5827951174752b8e2998453
Kathryn Morgan <usrbinkat@braincraft.io>
2021-12-19 23:32:22 -0800

    Update entrypoint.sh

--- containers/kubespray/rootfs/entrypoint.sh
+++ containers/kubespray/rootfs/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -x
-# ansible-playbook --user fedora -e ansible_ssh_pass=fedora -e ansible_sudo_pass=fedora --become ./cluster.yml
-# /entrypoint.sh --user fedora -e ansible_ssh_pass=fedora -e ansible_sudo_pass=fedora
 
 declare -a IPS="${HOSTS}"
 
@@ -23,13 +21,9 @@ ansible-playbook $@ --extra-vars @/etc/ansible/vars.yml --become ./cluster.yml |
 cat /etc/ansible/artifacts/admin.conf > /root/.kube/config
 kubectl scale deployment --replicas=0 dns-autoscaler --namespace=kube-system
 kubectl apply -f /root/patch/multus-daemonset.yml
-kubectl taint nodes --all --overwrite node-role.kubernetes.io/master-
-kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/master=''
-kubectl label nodes node1 node2 node3 --overwrite node-role.kubernetes.io/control-plane=''
-kubectl label nodes --all --overwrite node-role.kubernetes.io/worker=''
 #kubectl patch deployment -n kube-system coredns --patch='{"spec":{"template":{"spec":{"tolerations":[]}}}}'
 #kubectl -n kube-system rollout restart deployment/coredns
 sleep 7
 kubectl apply -f /root/patch/multus-daemonset.yml
 kubectl get po -A
-cat /etc/ansible/artifacts/admin.conf | sed "s/$VRRP_IP/$KUBE_API_FQDN/g" | tee /config
+cat /etc/ansible/artifacts/admin.conf > /config
